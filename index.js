08d2b41ed026c8e3ed10f22a2479581855bc9cfb
tunnckoCore <mameto_100@mail.bg>
2016-11-07 20:58:59 +0200

    chore(init): implement

--- index.js
+++ index.js
@@ -7,8 +7,18 @@
 
 'use strict'
 
+var collectionVisit = require('collection-visit')
+var isRegistered = require('minibase-is-registered')
+
 module.exports = function minibaseVisit (opts) {
   return function minibaseVisit (self) {
+    self.use(isRegistered())
+    /* istanbul ignore next */
+    if (self.isRegistered('visit')) return
 
+    self.define('visit', function visit (method, value) {
+      collectionVisit(self, method, value)
+      return self
+    })
   }
 }
--- test.js
+++ test.js
@@ -10,7 +10,27 @@
 'use strict'
 
 var test = require('mukla')
+var visit = require('./index')
+var MiniBase = require('minibase').MiniBase
 
-test(function (done) {
+test('should have `.visit` method', function (done) {
+  var app = new MiniBase()
+  app.use(visit())
+  test.strictEqual(typeof app.visit, 'function')
+  done()
+})
+
+test('should visit method', function (done) {
+  var ctx = {}
+  var base = new MiniBase()
+  base.use(visit())
+
+  base.foobar = function foobar (key, val) {
+    ctx[key] = val
+  }
+
+  base.visit('foobar', { aaa: 123, deep: { bb: 'cc' } })
+  test.strictEqual(ctx.aaa, 123)
+  test.strictEqual(ctx.deep.bb, 'cc')
   done()
 })
