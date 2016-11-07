d9c138a985cbfb86dc6e29f68e66a820730de0fb
tunnckoCore <mameto_100@mail.bg>
2016-11-07 20:35:26 +0200

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+/*!
+ * minibase-visit <https://github.com/node-minibase/minibase-visit>
+ *
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = function minibaseVisit (opts) {
+  return function minibaseVisit (self) {
+
+  }
+}
---
+++ test.js
@@ -0,0 +1,16 @@
+/*!
+ * minibase-visit <https://github.com/node-minibase/minibase-visit>
+ *
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+/* jshint asi:true */
+
+'use strict'
+
+var test = require('mukla')
+
+test(function (done) {
+  done()
+})
